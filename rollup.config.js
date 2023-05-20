509840eb0c800191bc97efbd1ab647b03573cb0d
tangmouren <1678977488@qq.com>
2023-05-20 14:58:53 +0800

    chore: init local env

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import { babel } from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'src/index.js', // 打包入口
+  output: [
+    {
+      file: 'lib/sparrow.js', // 对于 Nodejs，打包成 commonjs
+      format: 'cjs',
+    },
+    {
+      file: 'esm/sparrow.js', // 对于浏览器，打包成 ES module
+      format: 'es',
+    },
+    {
+      file: 'dist/sparrow.min.js',
+      name: 'sp',
+      format: 'umd', // 对于 Nodejs 和浏览器，打包成混合模式
+    },
+  ],
+  plugins: [nodeResolve(), babel({ babelHelpers: 'bundled' })],
+};
